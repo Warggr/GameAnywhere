8464a825a9fceb46c6a16db304869a5554ab9074
Pierre Ballif <pierre@ballif.eu>
2025-02-14 12:39:22 +0100

    add a setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='game_anywhere',
+    version='1.0',
+    packages=['game_anywhere', 'game_anywhere.core',
+              'game_anywhere.components'],
+    url='',
+    license='',
+    author='Pierre Ballif',
+    author_email='pierre.ballif@tum.de',
+    description='A framework for tabletop games.'
+)
